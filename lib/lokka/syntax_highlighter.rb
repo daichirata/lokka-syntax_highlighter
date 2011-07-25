a0f15fc7d7c78815ef95174bdfbee159423d50a3
Daic_h <bunny.hop.md@gmail.com>
2011-07-26 00:18:37 +0900

    add change syntax highlit theme and i18n

--- lib/lokka/syntax_highlighter.rb
+++ lib/lokka/syntax_highlighter.rb
@@ -1,20 +1,29 @@
 module Lokka
   module SyntaxHighlighter
     def self.registered(app)
+      app.get '/admin/plugins/syntax_highlighter' do
+        haml :"plugin/lokka-syntax_highlighter/views/index", :layout => :"admin/layout"
+      end
+
+      app.put '/admin/plugins/syntax_highlighter' do
+        Option.syntax_highlighter_theme = params['syntax_highlighter_theme']
+        flash[:notice] = 'Updated.'
+        redirect '/admin/plugins/syntax_highlighter'
+      end
+
       app.before do
-      #  if ENV['RACK_ENV'] == 'production' && !logged_in?
-          app.get '/admin/plugins/syntax_highlighter' do
-            haml :"plugin/lokka-syntax_highlighter/views/index", :layout => :"admin/layout"
-          end
+        theme = Option.syntax_highlighter_theme || "Default"
+        header = []
+        header << %Q(<link type="text/css" rel="Stylesheet" href="/plugin/lokka-syntax_highlighter/views/styles/shCore.css"/>)
+        header << %Q(<link type="text/css" rel="Stylesheet" href="/plugin/lokka-syntax_highlighter/views/styles/shTheme#{theme}.css"/>)
 
-          content_for :header do
-            haml :'plugin/lokka-syntax_highlighter/views/header', :layout => false
-          end
+        content_for :header do
+          header
+        end
 
-          content_for :footer do
-            haml :'plugin/lokka-syntax_highlighter/views/footer', :layout => false
-          end
-      #  end
+        content_for :footer do
+          haml :'plugin/lokka-syntax_highlighter/views/footer', :layout => false
+        end
       end
     end
   end
